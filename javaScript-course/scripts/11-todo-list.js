e33abef82a97ce1514f692e90b7325170d7ed443
abanoubg1 <abanoubg94@outlook.com>
2024-07-05 20:20:12 +0300

    to todolistpart2 and check the previous

--- javaScript-course/scripts/11-todo-list.js
+++ javaScript-course/scripts/11-todo-list.js
@@ -3,5 +3,29 @@ function addTodo () {
     const inputElement = document.querySelector('.js-name-input');
     const name = inputElement.value;
     todoArray.push(name);
+    inputElement.value = '';
     console.log(todoArray);
 }
+const todoList = ['makedinner','wash dishes'];
+
+renderTodoList();
+function renderTodoList() {
+    
+    let todoListHTML = '';
+    for (let i = 0; i< todoList.length; i++) {
+        const todo = todoList[i];
+        const html = `<p>${todo}</P>`;
+        todoListHTML += html;
+    }
+    console.log(todoListHTML);
+    document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+}
+
+function addTodo2 () {
+    const inputElement = document.querySelector('.js-name-input2');
+    const name = inputElement.value;
+    todoList.push(name);
+    inputElement.value = '';
+    renderTodoList();
+}
+
